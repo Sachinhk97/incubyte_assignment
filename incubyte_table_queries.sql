c567aabcf11bc2a6dceeb552d4747c9be8f71cf0
Sachin H K <56583837+Sachinhk97@users.noreply.github.com>
2021-04-29 18:53:26 +0530

    Update incubyte_table_queries.sql

--- incubyte_table_queries.sql
+++ incubyte_table_queries.sql
@@ -12,19 +12,19 @@ CREATE TABLE Table_source (
     active_customer CHAR(1) 
 );
 
-INSERT INTO Table_source(customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
+INSERT INTO Table_source (customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
 VALUES ('Alex',123457,20101012,20121013,'MVD','Paul','SA','USA',06031987,'A');
 
-INSERT INTO Table_source(customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
+INSERT INTO Table_source (customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
 VALUES ('John',123458,20101012,20121013,'MVD','Paul','TN','IND',06031987,'A');
 
-INSERT INTO Table_source(customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
+INSERT INTO Table_source (customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
 VALUES ('Mathew',123459,20101012,20121013,'MVD','Paul','WAS','PHIL',06031987,'A');
 
-INSERT INTO Table_source(customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
+INSERT INTO Table_source (customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
 VALUES ('Matt',12345,20101012,20121013,'MVD','Paul','BOS','NYC',06031987,'A');
 
-INSERT INTO Table_source(customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
+INSERT INTO Table_source (customer_name, customer_id, customer_open_date, last_consulted_date, vaccination_type, doctor_consulted, state, country, date_of_birth, active_customer) 
 VALUES ('Jacob',1256,20101012,20121013,'MVD','Paul','VIC','AU',06031987,'A');
 
 CREATE_TABLE Table_India AS SELECT * FROM Table_source;
